e39d7004e0176ed546ef7c55ce5c1091ec8095a0
Lin Guangting <158265375+cxksingdancerap@users.noreply.github.com>
2024-08-23 16:59:41 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+class person
+{
+	int m_a;
+	static int m_b;
+	void func1()
+	{
+
+	}
+	static void func2()
+	{
+
+	}
+};
+void test01()
+{
+	person p1;
+	cout << "sizeof(p1)=" << sizeof(p1) << endl;
+}
+int main()
+{
+	test01();
+	system("pause");
+	return 0;
+}
